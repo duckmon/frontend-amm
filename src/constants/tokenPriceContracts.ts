3a388bd6357583ce7a9744dcc4e71ec3c62bda42
Code <code@localhost>
2021-03-05 22:36:09 +0100

    lp address

--- src/constants/tokenPriceContracts.ts
+++ src/constants/tokenPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0x91918f3EC1F030E2b1Bd2B3aaEcF6760d46258d9',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19c8f9570a342268abe2bf868617cdd7730b6abe'
+  lpAddress: '0x3b375d727f3a16359919494f7bb02479c96c0a7e'
 }
 
 export default priceContracts
